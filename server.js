8b698e666b42644cda34615bf14895236f460d2f
Sai Banda <sai.banda@aspiresys.com>
2024-10-15 21:37:56 +0530

    add backend

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors = require('cors');
+const { db } = require('./db/db');
+const {readdirSync} = require('fs')
+const app = express()
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+// const PORT = "https://expense-test.vercel.app"
+
+//middlewares
+app.use(express.json())
+const corsConfig = {
+    origin: '',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
+
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+const server = () => {
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT)
+    })
+}
+
+server()
